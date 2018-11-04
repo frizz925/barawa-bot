11ffc751a58b37d72f1c6a1eca1ea3b8fb094894
Izra Faturrahman <Frizz925@hotmail.com>
2018-11-04 23:07:27 +0700

    Mitigate overflow

--- lib/rand_from_string.go
+++ lib/rand_from_string.go
@@ -2,10 +2,11 @@ package lib
 
 import "time"
 
+// RandFromString Placeholder comment
 func RandFromString(text string, params ...interface{}) int64 {
 	result := int64(0)
 	for _, char := range text {
-		result += int64(char)
+		result = (result + int64(char)) % 8096
 	}
 	if len(params) >= 1 {
 		result += int64(params[0].(int))
