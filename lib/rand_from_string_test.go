125ed3c724f175478020c5a169bb000071b9069a
Izra Faturrahman <Frizz925@hotmail.com>
2018-11-04 22:51:42 +0700

    Improve test coverage

--- lib/rand_from_string_test.go
+++ lib/rand_from_string_test.go
@@ -5,7 +5,9 @@ import (
 )
 
 func TestRandFromString(t *testing.T) {
-	result := RandFromString("abc", 0)
+	// TODO: make assertion for RandFromString without 2nd param
+	result := RandFromString("abc")
+	result = RandFromString("abc", 0)
 	if result != 294 {
 		panic("Result from RandFromString(\"abc\") should be 294")
 	}
